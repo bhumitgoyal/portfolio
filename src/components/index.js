ee8938104455ba25d664551da1bb04d365870dc8
bhumitgoyal <51223177+bhumitgoyal@users.noreply.github.com>
2025-02-27 16:03:56 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import { MobileMenu } from './mobilemenu';
+import { MobileMenu } from './MobileMenu';
 
 export {
   Hero,
